ac90fd5cd229bfb58947d78b222f0b5b1dc6947f
Ravi <145011934+Ravipanchal0@users.noreply.github.com>
2023-10-29 09:51:55 +0530

    add favicon and update contact page and fixed some bugs

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ app.set("view engine", "pug"); // Set the template engine as pug
 app.set("views", path.join(__dirname, "views")); // Set the views directory
 
 app.get("/", (req, res) => {
-  para = {};
+  para = { title: "Welcome to Ravi Panchal's Digital Canvas" };
   res.status(200).render("home.pug", para);
 });
 app.get("/about", (req, res) => {
-  para = {};
+  para = { title: "Get to Know Ravi Panchal" };
   res.status(200).render("about.pug", para);
 });
 app.get("/resume", (req, res) => {
-  para = {};
+  para = { title: "My Digital Journey" };
   res.status(200).render("resume.pug", para);
 });
 app.get("/portfolio", (req, res) => {
-  para = {};
+  para = { title: "My Digital Showcase" };
   res.status(200).render("portfolio.pug", para);
 });
 app.get("/blog", (req, res) => {
-  para = {};
+  para = { title: "Insights from Ravi Panchal" };
   res.status(200).render("blog.pug", para);
 });
 app.get("/contact", (req, res) => {
-  para = {};
+  para = { title: "Let's Connect to Ravi Panchal" };
   res.status(200).render("contact.pug", para);
 });
 
