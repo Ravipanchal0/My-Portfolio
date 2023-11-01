8a39ac578c6d640acef16c521f7b0e0a30ca432f
Ravi <145011934+Ravipanchal0@users.noreply.github.com>
2023-11-01 08:31:50 +0530

    netlify packages insatll for deploy

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
 const express = require("express");
 const path = require("path");
+const pug = require("pug");
+const fs = require("fs");
 
 const app = express();
 const port = 8000;
 
 app.use("/static", express.static("static"));
 
-app.set("view engine", "pug"); // Set the template engine as pug
 app.set("views", path.join(__dirname, "views")); // Set the views directory
+app.set("view engine", "pug"); // Set the template engine as pug
 
 app.get("/", (req, res) => {
-  para = { title: "Welcome to Ravi Panchal's Digital Canvas" };
-  res.status(200).render("home.pug", para);
+  const html = pug.renderFile("views/home.pug"); // Replace 'index.pug' with your actual Pug file
+  fs.writeFileSync("public/home.html", html); // Save the rendered HTML to a 'public' directory
+  res.send(html);
 });
+
 app.get("/about", (req, res) => {
-  para = { title: "Get to Know Ravi Panchal" };
-  res.status(200).render("about.pug", para);
+  const html = pug.renderFile("views/about.pug"); // Replace 'index.pug' with your actual Pug file
+  fs.writeFileSync("public/about.html", html); // Save the rendered HTML to a 'public' directory
+  res.send(html);
 });
+
 app.get("/resume", (req, res) => {
-  para = { title: "My Digital Journey" };
-  res.status(200).render("resume.pug", para);
+  const html = pug.renderFile("views/resume.pug"); // Replace 'index.pug' with your actual Pug file
+  fs.writeFileSync("public/resume.html", html); // Save the rendered HTML to a 'public' directory
+  res.send(html);
 });
+
 app.get("/portfolio", (req, res) => {
-  para = { title: "My Digital Showcase" };
-  res.status(200).render("portfolio.pug", para);
+  const html = pug.renderFile("views/portfolio.pug"); // Replace 'index.pug' with your actual Pug file
+  fs.writeFileSync("public/portfolio.html", html); // Save the rendered HTML to a 'public' directory
+  res.send(html);
 });
+
 app.get("/blog", (req, res) => {
-  para = { title: "Insights from Ravi Panchal" };
-  res.status(200).render("blog.pug", para);
+  const html = pug.renderFile("views/blog.pug"); // Replace 'index.pug' with your actual Pug file
+  fs.writeFileSync("public/blog.html", html); // Save the rendered HTML to a 'public' directory
+  res.send(html);
 });
+
 app.get("/contact", (req, res) => {
-  para = { title: "Let's Connect to Ravi Panchal" };
-  res.status(200).render("contact.pug", para);
+  const html = pug.renderFile("views/contact.pug"); // Replace 'index.pug' with your actual Pug file
+  fs.writeFileSync("public/contact.html", html); // Save the rendered HTML to a 'public' directory
+  res.send(html);
 });
-app.get("*", function (req, res) {
-  res.status(404).render("404.pug");
+
+app.get("*", (req, res) => {
+  const html = pug.renderFile("views/404.pug"); // Replace 'index.pug' with your actual Pug file
+  fs.writeFileSync("public/404.html", html); // Save the rendered HTML to a 'public' directory
+  res.send(html);
 });
 
 app.listen(port, () => {
